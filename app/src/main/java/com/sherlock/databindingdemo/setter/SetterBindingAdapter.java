5e208f356531f3dd8a9680e34ae0753c25130f4f
xulei <xulei964270141@qq.com>
2017-04-20 17:55:58 +0800

    添加BindConversion

--- app/src/main/java/com/sherlock/databindingdemo/setter/SetterBindingAdapter.java
+++ app/src/main/java/com/sherlock/databindingdemo/setter/SetterBindingAdapter.java
@@ -1,9 +1,15 @@
 package com.sherlock.databindingdemo.setter;
 
 import android.databinding.BindingAdapter;
+import android.databinding.BindingConversion;
+import android.databinding.BindingMethod;
+import android.databinding.BindingMethods;
+import android.databinding.InverseBindingAdapter;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -14,11 +20,34 @@ import com.squareup.picasso.Picasso;
  */
 
 public class SetterBindingAdapter {
-    //不能加app，加上会报错找不到setter
-//    @BindingAdapter({"app:imageUrl", "app:placeholder"})
+
+//    @BindingMethods({@BindingMethod(
+//            type = "android.widget.ImageView", 
+//            attribute = "android:tint",
+//            method = "setImageTintList")
+//    })
+
+    /**
+     * ImageView绑定图片url
+     */
     @BindingAdapter({"imageUrl", "placeholder"})
     public static void setImageUrl(ImageView view, String url, Drawable drawable) {
         Picasso.with(view.getContext()).load(url).placeholder(drawable).into(view);
         Log.e("imageUrl", url);
+        //@BindingAdapter({"app:imageUrl", "app:placeholder"})
+        //不能加app，加上会报错找不到setter
+    }
+
+    /**
+     * color转Drawable
+     */
+    @BindingConversion
+    public static ColorDrawable transformColorToDrawable(int color) {
+        return new ColorDrawable(color);
+    }
+
+    @InverseBindingAdapter(attribute = "android:text")
+    public static String getTextString(TextView view) {
+        return view.getText().toString();
     }
 }
